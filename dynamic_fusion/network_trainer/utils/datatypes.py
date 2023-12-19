0060cc9e5ba8c8461536576e37181716ae6ca7c3
David Tonderski <dtonderski@gmail.com>
2023-12-19 13:05:34 +0100

    docs: document Batch, fix video

--- dynamic_fusion/network_trainer/utils/datatypes.py
+++ dynamic_fusion/network_trainer/utils/datatypes.py
@@ -12,13 +12,13 @@ class ReconstructionSample:
     timestamp_means: Float32[torch.Tensor, "Time SubBin X Y"]
     timestamp_stds: Float32[torch.Tensor, "Time SubBin X Y"]
     event_counts: Float32[torch.Tensor, "Time SubBin X Y"]
-    video: Float32[torch.Tensor, "Time SubBin X Y"]
+    video: Float32[torch.Tensor, "Time 1 X Y"]
 
 
 Batch: TypeAlias = Tuple[
-    Float32[torch.Tensor, "batch Time SubBin X Y"],
-    Float32[torch.Tensor, "batch Time SubBin X Y"],
-    Float32[torch.Tensor, "batch Time SubBin X Y"],
-    Float32[torch.Tensor, "batch Time SubBin X Y"],
-    Float32[torch.Tensor, "batch Time 1 X Y"],
+    Float32[torch.Tensor, "batch Time SubBin X Y"], # EPS
+    Float32[torch.Tensor, "batch Time SubBin X Y"], # Means
+    Float32[torch.Tensor, "batch Time SubBin X Y"], # STD
+    Float32[torch.Tensor, "batch Time SubBin X Y"], # Counts
+    Float32[torch.Tensor, "batch Time 1 X Y"],      # Video
 ]
