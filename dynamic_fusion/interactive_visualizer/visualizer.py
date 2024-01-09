9dd42e8124ad1fc135027ebdd7f0aa67463497a5
David Tonderski <dtonderski@gmail.com>
2024-01-09 18:34:19 +0100

    feat: add zoom

--- dynamic_fusion/interactive_visualizer/visualizer.py
+++ dynamic_fusion/interactive_visualizer/visualizer.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import Any
+from typing import Any, Optional
 
 import customtkinter as ctk
 from customtkinter import filedialog
@@ -15,7 +15,7 @@ from PIL import Image, ImageTk, ImageDraw
 NO_DATA_TEXT = "No data folder selected!"
 START_BIN_INDEX_TEMPLATE = "Start bin index: {val:.0f}"
 END_BIN_INDEX_TEMPLATE = "End bin index: {val:.0f}"
-GRID_LINES = 5
+GRID_LINES_PIXEL_SPACING = 20
 TIMESTAMP_TEMPLATE = "Timestamp in bin: {val:.2f}"
 GT_TEMPLATE = "Ground truth at: {val:.2f}"
 PREDICTION_TEMPLATE = "Reconstruction at {val:.2f}"
@@ -25,20 +25,26 @@ ctk.set_appearance_mode("dark")
 
 
 class Visualizer(ctk.CTk):  # type: ignore
+    config: VisualizerConfiguration
     row: int = 0
     network_handler: NetworkHandler
     start_image: ImageTk.PhotoImage
     end_image: ImageTk.PhotoImage
+    x_start: Optional[int] = None
+    x_stop: Optional[int] = None
+    y_start: Optional[int] = None
+    y_stop: Optional[int] = None
 
     def __init__(self, configuration: VisualizerConfiguration) -> None:
         super().__init__()
+        self.config = configuration
         self.network_handler = NetworkHandler(
             configuration.network_handler, configuration.network_loader
         )
 
         # configure window
         self.title("CustomTkinter complex_example.py")
-        self.geometry(f"{1500}x{800}")
+        self.geometry(f"{1800}x{950}")
 
         # configure grid layout (3x3)
         self.grid_columnconfigure((0, 1, 2), weight=1)
@@ -78,7 +84,7 @@ class Visualizer(ctk.CTk):  # type: ignore
         self.start_bin_slider = ctk.CTkSlider(
             self.bins_frame,
             from_=0,
-            to=99,
+            to=self.config.total_bins_in_video-1,
             variable=self.start_bin_var,
             width=300,
             command=command,
@@ -99,7 +105,7 @@ class Visualizer(ctk.CTk):  # type: ignore
         self.end_bin_slider = ctk.CTkSlider(
             self.bins_frame,
             from_=0,
-            to=99,
+            to=self.config.total_bins_in_video-1,
             variable=self.end_bin_var,
             width=300,
             command=command,
@@ -117,6 +123,37 @@ class Visualizer(ctk.CTk):  # type: ignore
         )
         self.timestamp_slider.grid(row=self._next_row(), column=0, columnspan=3)
         self.timestamp_slider.bind("<ButtonRelease-1>", self._new_slider_value)
+
+        # Zoom
+        self.zoom_frame = ctk.CTkFrame(self)
+        self.zoom_frame.grid(row = self._next_row(), column=0, columnspan=3)
+
+        self.x_start_label = ctk.CTkLabel(self.zoom_frame, text="x_start")
+        self.x_start_label.grid(row=0, column=0)
+        self.x_start_string = ctk.StringVar(self)
+        self.x_start_entry = ctk.CTkEntry(self.zoom_frame, textvariable=self.x_start_string)
+        self.x_start_entry.grid(row=0, column=1, padx=2)
+
+        self.x_stop_label = ctk.CTkLabel(self.zoom_frame, text="x_stop")
+        self.x_stop_label.grid(row=0, column=2)
+        self.x_stop_string = ctk.StringVar(self)
+        self.x_stop_entry = ctk.CTkEntry(self.zoom_frame, textvariable=self.x_stop_string)
+        self.x_stop_entry.grid(row=0, column=3, padx=2)
+
+        self.y_start_label = ctk.CTkLabel(self.zoom_frame, text="y_start")
+        self.y_start_label.grid(row=0, column=4)
+        self.y_start_string = ctk.StringVar(self)
+        self.y_start_entry = ctk.CTkEntry(self.zoom_frame, textvariable=self.y_start_string)
+        self.y_start_entry.grid(row=0, column=5, padx=2)
+
+        self.y_stop_label = ctk.CTkLabel(self.zoom_frame, text="y_stop")
+        self.y_stop_label.grid(row=0, column=6)
+        self.y_stop_string = ctk.StringVar(self)
+        self.y_stop_entry = ctk.CTkEntry(self.zoom_frame, textvariable=self.y_stop_string)
+        self.y_stop_entry.grid(row=0, column=7, padx=2)
+
+        self.apply_zoom_button = ctk.CTkButton(self.zoom_frame, text="Apply zoom", command=self._apply_zoom)
+        self.apply_zoom_button.grid(row=0,column=9)
 
         # Images
         self.start_image_label = ctk.CTkLabel(
@@ -173,26 +210,28 @@ class Visualizer(ctk.CTk):  # type: ignore
             self.start_bin_var.get(), self.end_bin_var.get()
         )
         # Start and end
-        start, end = self.network_handler.get_start_and_end_images()
-        self.start_image = Image.fromarray(start * 255)
-        self.end_image = Image.fromarray(end * 255)
-        print("Got start and end images!")
         self._update_start_and_end_images()
         # GT
         self._update_gt()
         # Events
-        event_image = self.network_handler.get_event_image()
-        self.event_image = Image.fromarray((event_image*255).astype(np.uint8))
         self._update_event_image()
         # Prediction
         self._update_prediction()
 
     def _update_start_and_end_images(self) -> None:
-        start_image = self.start_image.convert("RGB")
-        end_image = self.end_image.convert("RGB")
+        start, end = self.network_handler.get_start_and_end_images()
+        start = start[self.x_start:self.x_stop, self.y_start:self.y_stop]
+        end = end[self.x_start:self.x_stop, self.y_start:self.y_stop]
+
+        start_image = Image.fromarray(start * 255)
+        end_image = Image.fromarray(end * 255)
+        start_image = start_image.convert("RGB")
+        end_image = end_image.convert("RGB")
 
         self._add_grid(start_image)
         self._add_grid(end_image)
+        start_image = start_image.resize(IMAGE_SIZE, resample=Image.BOX)
+        end_image = end_image.resize(IMAGE_SIZE, resample=Image.BOX)
 
         start_image = ctk.CTkImage(start_image, size=IMAGE_SIZE)
         end_image = ctk.CTkImage(end_image, size=IMAGE_SIZE)
@@ -202,9 +241,13 @@ class Visualizer(ctk.CTk):  # type: ignore
         self.end_image_label.image = end_image
     
     def _update_event_image(self) -> None:
+        event_image = self.network_handler.get_event_image()
+        event_image = event_image[self.x_start:self.x_stop, self.y_start:self.y_stop]
+        self.event_image = Image.fromarray((event_image*255).astype(np.uint8))
         event_image = self.event_image.copy()
 
         self._add_grid(event_image)
+        event_image = event_image.resize(IMAGE_SIZE, resample=Image.BOX)
 
         event_image = ctk.CTkImage(event_image, size=IMAGE_SIZE)
         self.event_image_label.configure(image=event_image)
@@ -214,12 +257,15 @@ class Visualizer(ctk.CTk):  # type: ignore
     def _update_gt(self) -> None:
         print(f"Updating GT to {self.timestamp_slider._value}")
         gt_image_np = self.network_handler.get_ground_truth(
-            self.timestamp_slider._value
+            self.timestamp_slider._value, self.config.total_bins_in_video
         )
+
+        gt_image_np = gt_image_np[self.x_start:self.x_stop, self.y_start:self.y_stop]
         gt_image = Image.fromarray(gt_image_np * 255).convert("RGB")
         self._add_grid(gt_image)
+        gt_image = gt_image.resize(IMAGE_SIZE, resample=Image.BOX)
 
-        gt_image = ctk.CTkImage(gt_image, size=IMAGE_SIZE)
+        gt_image = ImageTk.PhotoImage(gt_image, size=IMAGE_SIZE)
         self.gt_image_label.configure(
             image=gt_image,
             text=GT_TEMPLATE.format(val=self.timestamp_slider._value),
@@ -235,13 +281,17 @@ class Visualizer(ctk.CTk):  # type: ignore
         # Grid dimensions (number of rows and columns)
 
         # Drawing the grid
-        for i in range(1, GRID_LINES):
+        for i, x in enumerate(range(0, width, GRID_LINES_PIXEL_SPACING)):
             fill = "red" if i % 2 == 0 else "blue"
-            y = i * height / GRID_LINES
-            x = i * width / GRID_LINES
+
+            draw.line([(x, 0), (x, height)], fill=fill)
+
+        for i, y in enumerate(range(0, height, GRID_LINES_PIXEL_SPACING)):
+            fill = "red" if i % 2 == 0 else "blue"
 
             draw.line([(0, y), (width, y)], fill=fill)
             draw.line([(x, 0), (x, height)], fill=fill)
+
 
     def _timestamp_slider_changed(self, value: float) -> None:
         self.timestamp_slider_label.configure(text=TIMESTAMP_TEMPLATE.format(val=value))
@@ -257,8 +307,11 @@ class Visualizer(ctk.CTk):  # type: ignore
         print(f"Prediction range: {prediction.min()} to {prediction.max()}")
         prediction[prediction < 0] = 0
         prediction[prediction > 1] = 1
+
+        prediction = prediction[self.x_start:self.x_stop, self.y_start:self.y_stop]
         prediction_image = Image.fromarray(prediction.numpy() * 255).convert("RGB")
         self._add_grid(prediction_image)
+        prediction_image = prediction_image.resize(IMAGE_SIZE, resample=Image.BOX)
 
         prediction_image = ctk.CTkImage(prediction_image, size=IMAGE_SIZE)
         self.prediction_label.configure(
@@ -289,10 +342,28 @@ class Visualizer(ctk.CTk):  # type: ignore
 
     def _new_bin_slider_value(self, _: Any) -> None:
         print(
-            f"Start: {self.start_bin_slider._value*99:.0f}, end:"
-            f" {self.end_bin_slider._value*99:.0f}"
+            f"Start: {self.start_bin_slider._value*(self.config.total_bins_in_video-1):.0f}, end:"
+            f" {self.end_bin_slider._value*(self.config.total_bins_in_video-1):.0f}"
         )
 
+    def _apply_zoom(self) -> None:
+        self.apply_zoom_button.focus_set()
+        try:
+            self.x_start = int(self.x_start_string.get())
+            self.x_stop = int(self.x_stop_string.get())
+            self.y_start = int(self.y_start_string.get())
+            self.y_stop = int(self.y_stop_string.get())
+            print("Integers parsed!")
+            self._update_gt()
+            self._update_prediction()
+            self._update_event_image()
+            self._update_start_and_end_images()
+        except Exception:
+            self.x_start = None
+            self.x_stop = None
+            self.y_start = None
+            self.y_stop = None
+            print("Integer parsing failed!")
 
 if __name__ == "__main__":
     with open(
