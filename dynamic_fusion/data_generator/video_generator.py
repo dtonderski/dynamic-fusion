refactor: remove unused code, fix rotation matrix

dynamic_fusion/data_generator/video_generator.py
@@ -35,7 +35,7 @@ class VideoGenerator:
         else:
             self.logger.info("Generating video...")
 
-        transform_definition = self._define_transforms(image.shape)
+        transform_definition = self._define_transforms()
 
         video_frame_times = np.linspace(
             0, 1, self.shared_config.number_of_images_to_generate_per_input
@@ -46,20 +46,13 @@ class VideoGenerator:
             transform_definition,
             video_frame_times,
             self.shared_config.target_image_size,
-            self.shared_config.number_of_images_to_generate_per_input,
-            fill_mode=self.config.pytorch_fill_mode  # type: ignore
+            fill_mode=self.config.fill_mode,  # type: ignore
+            interpolation=self.config.interpolation,  # type: ignore
         )
         return video, transform_definition
 
-    def _define_transforms(
-        self, image_resolution: Optional[Tuple[int, int]] = None
-    ) -> TransformDefinition:
-        if self.shared_config.target_image_size is None and image_resolution is None:
-            raise ValueError("Resolution must be set if target_image_size is None!")
-
-        shift_knots, rotation_knots, scale_knots = self._generate_knots(
-            image_resolution
-        )
+    def _define_transforms(self) -> TransformDefinition:
+        shift_knots, rotation_knots, scale_knots = self._generate_knots()
         shift_interpolation, rotation_interpolation, scale_interpolation = (
             self._generate_interpolation_type() for _ in range(3)
         )
@@ -72,27 +65,16 @@ class VideoGenerator:
             scale_interpolation,
         )
 
-    def _generate_knots(
-        self, image_resolution: Optional[Tuple[int, int]] = None
-    ) -> Tuple[
+    def _generate_knots(self) -> Tuple[
         Float[np.ndarray, "NShiftKnots 2"],
         Float[np.ndarray, "NRotKnots 1"],
         Float[np.ndarray, "NScaleKnots 2"],
     ]:
-        if self.shared_config.target_image_size is None and image_resolution is None:
-            raise ValueError("Resolution must be set if target_image_size is None!")
-
         number_of_shift_knots = randint(
             low=2, high=self.config.max_number_of_shift_knots, dtype=np.int32
         )
 
-        if self.shared_config.target_image_size is not None:
-            image_resolution = self.shared_config.target_image_size
-
-        max_shift_knot_value = (
-            np.array(self.config.max_shift_knot_multiplier_value)
-            * image_resolution
-        )
+        max_shift_knot_value = np.array(self.config.max_shift_knot_multiplier_value)
         shift_knot_values = uniform(size=(number_of_shift_knots, 2)) * np.reshape(
             max_shift_knot_value, (1, 2)
         )

dynamic_fusion/utils/video.py
@@ -24,10 +24,9 @@ def get_video(
     transform_definition: TransformDefinition,
     timestamps: Float[np.ndarray, " T"],
     target_image_size: Optional[Tuple[int, int]],
-    number_of_images_to_generate_per_input: Optional[int] = None,
-    use_pytorch: bool = True,
     device: torch.device = torch.device("cuda"),
-    fill_mode: Literal["wrap", "zeros", "border", "reflection"] = "wrap"
+    fill_mode: Literal["wrap", "zeros", "border", "reflection"] = "wrap",
+    interpolation: Literal["bilinear","nearest","bicubic"] = "bicubic",
 ) -> GrayVideoFloat:
     """Used to generate images at arbitrary timestamps from an initial
     image and a transform definition. Note that the time of the video
@@ -49,72 +48,24 @@ def get_video(
     shifts, rotations, scales = _interpolate_transforms(
         transform_definition, timestamps
     )
-    video = _generate_video(
-        image,
-        shifts,
-        rotations,
-        scales,
-        target_image_size,
-        number_of_images_to_generate_per_input,
-        use_pytorch,
-        device,
-        fill_mode
+
+    transformation_matrices = _transforms_to_matrices(shifts, rotations, scales)
+
+    grid = torch.nn.functional.affine_grid(
+        torch.tensor(transformation_matrices[:, :2, :], device=device),
+        [shifts.shape[0], 1, *image.shape],
     )
-    return video
+    if fill_mode == "wrap":
+        # Need to do in-place for memory concerns
+        grid.add_(1).remainder_(2).subtract_(1)
+        fill_mode = "zeros"
 
-
-def _generate_video(
-    image: GrayImage,
-    shifts: Float[np.ndarray, "T 2"],
-    rotations: Float[np.ndarray, "T 1"],
-    scales: Float[np.ndarray, "T 2"],
-    target_image_size: Optional[Tuple[int, int]],
-    number_of_images_to_generate_per_input: Optional[int] = None,
-    use_pytorch: bool = True,
-    device: torch.device = torch.device("cuda"),
-    fill_mode: Literal["wrap", "zeros", "border", "reflection"] = "wrap"
-) -> GrayVideoFloat:
-    if use_pytorch:
-        transformation_matrices = _transforms_to_matrices(
-            image,
-            shifts / target_image_size,
-            rotations,
-            scales,
-            shifts.shape[0],
-            False,
-            invert=True
-        )
-
-        grid = torch.nn.functional.affine_grid(
-            torch.tensor(transformation_matrices[:, :2, :], device=device),
-            [shifts.shape[0], 1, *image.shape],
-        )
-        if fill_mode == "wrap":
-            grid.add_(1).remainder_(2).subtract_(1)
-            fill_mode = "zeros"
-
-        image_tensor = einops.repeat(
-            torch.tensor(image, device=device, dtype=torch.float),
-            "H W -> N 1 H W",
-            N=shifts.shape[0],
-        )
-        video = torch.nn.functional.grid_sample(
-            image_tensor, grid, "nearest", fill_mode
-        )
-    else:
-        if number_of_images_to_generate_per_input is None:
-            raise ValueError(
-                "number_of_images_to_generate_per_input must be set if not using"
-                " pytorch!"
-            )
-        transformation_matrices = _transforms_to_matrices(
-            image,
-            shifts,
-            rotations,
-            scales,
-            number_of_images_to_generate_per_input,
-        )
-        video = _generate_video_scipy(image, transformation_matrices)
+    image_tensor = einops.repeat(
+        torch.tensor(image, device=device, dtype=torch.float),
+        "H W -> N 1 H W",
+        N=shifts.shape[0],
+    )
+    video = torch.nn.functional.grid_sample(image_tensor, grid, interpolation, fill_mode)
     video = normalize(video)
     if target_image_size is not None:
         video = crop_video(video, target_image_size)
@@ -186,113 +137,40 @@ def _upsample_knot_values(
     return interpolation(points_to_interpolate)
 
 
-def _transforms_to_torch(
-    shifts: Float[np.ndarray, "T 2"],
-    rotations: Float[np.ndarray, "T 1"],
-    scales: Float[np.ndarray, "T 2"],
-) -> Tuple[List[Tuple[int, int]], List[float], List[float]]:
-    angles = [float(rotation) * 180 / np.pi for rotation in rotations]
-    translates = [tuple(int(x) for x in shift) for shift in shifts]
-    torch_scales = [scale[0] for scale in scales]
-    return translates, angles, torch_scales  # type: ignore
-
-
 def _transforms_to_matrices(  # pylint: disable=R0913,R0914
-    image: GrayImage,
     shifts: Float[np.ndarray, "T 2"],
     rotations: Float[np.ndarray, "T 1"],
     scales: Float[np.ndarray, "T 2"],
-    number_of_images_to_generate_per_input: int,
-    rotate_around_center: bool = True,
-    invert: bool = False,
 ) -> Float[np.ndarray, "T 3 3"]:
-    x_size, y_size = image.shape
-    centering_matrix = np.array(
-        [[1, 0, x_size / 2.0], [0, 1, y_size / 2.0], [0, 0, 1]],
-        dtype=np.float32,
-    )
-    inverse_centering_matrix = np.array(
-        [[1, 0, -x_size / 2.0], [0, 1, -y_size / 2.0], [0, 0, 1]],
-        dtype=np.float32,
-    )
-
     transformation_matrices = np.zeros(
-        (number_of_images_to_generate_per_input, 3, 3),
+        (shifts.shape[0], 3, 3),
         dtype=np.float32,
     )
 
-    if not invert:
-        for step in range(number_of_images_to_generate_per_input):
-            theta = rotations[step, 0]
-            scale = scales[step, :]
-            shift = shifts[step, :]
+    for step, (shift, rotation, scale) in enumerate(zip(shifts, rotations, scales)):
+        theta = rotation[0]
 
-            rotation_matrix = np.array(
-                [
-                    [np.cos(theta), -np.sin(theta), 0],
-                    [np.sin(theta), np.cos(theta), 0],
-                    [0, 0, 1],
-                ],
-                dtype=np.float32,
-            )
-            shift_matrix = np.array(
-                [[1, 0, shift[0]], [0, 1, shift[1]], [0, 0, 1]],
-                dtype=np.float32,
-            )
+        rotation_matrix = np.array(
+            [
+                [np.cos(theta), -np.sin(theta), 0],
+                [np.sin(theta), np.cos(theta), 0],
+                [0, 0, 1],
+            ],
+            dtype=np.float32,
+        )
+        shift_matrix = np.array(
+            [[1, 0, -shift[0]], [0, 1, -shift[1]], [0, 0, 1]],
+            dtype=np.float32,
+        )
 
-            scale_matrix = np.array(
-                [[scale[0], 0, 0], [0, scale[1], 0], [0, 0, 1]],
-                dtype=np.float32,
-            )
+        scale_matrix = np.array(
+            [[1 / scale[0], 0, 0], [0, 1 / scale[1], 0], [0, 0, 1]],
+            dtype=np.float32,
+        )
 
-            if not rotate_around_center:
-                transformation_matrices[step, ...] = (
-                    rotation_matrix @ shift_matrix @ scale_matrix
-                )
-                continue
-            transformation_matrices[step, ...] = (
-                centering_matrix
-                @ rotation_matrix
-                @ inverse_centering_matrix
-                @ shift_matrix
-                @ scale_matrix
-            )
-    if invert:
-        for step in range(number_of_images_to_generate_per_input):
-            theta = rotations[step, 0]
-            scale = scales[step, :]
-            shift = shifts[step, :]
-
-            rotation_matrix = np.array(
-                [
-                    [np.cos(theta), np.sin(theta), 0],
-                    [-np.sin(theta), np.cos(theta), 0],
-                    [0, 0, 1],
-                ],
-                dtype=np.float32,
-            )
-            shift_matrix = np.array(
-                [[1, 0, -shift[0]], [0, 1, -shift[1]], [0, 0, 1]],
-                dtype=np.float32,
-            )
-
-            scale_matrix = np.array(
-                [[1 / scale[0], 0, 0], [0, 1 / scale[1], 0], [0, 0, 1]],
-                dtype=np.float32,
-            )
-
-            if not rotate_around_center:
-                transformation_matrices[step, ...] = (
-                    scale_matrix @ shift_matrix @ rotation_matrix
-                )
-                continue
-            transformation_matrices[step, ...] = (
-                scale_matrix
-                @ shift_matrix
-                @ centering_matrix
-                @ rotation_matrix
-                @ inverse_centering_matrix
-            )
+        transformation_matrices[step, ...] = (
+            scale_matrix @ shift_matrix @ rotation_matrix
+        )
 
     return transformation_matrices
 