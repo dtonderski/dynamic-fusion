f68f6f1e8ca5d7afa3880a618c82db8b763fe2dd
David Tonderski <dtonderski@gmail.com>
2024-01-27 19:02:16 +0100

    feat: add option to overwrite config
    for example:
    main.py --generate_data --config <path_to_config> --image_loader.file_extension=png

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import argparse
 import logging
+from typing import Any, List, Tuple
+from pydantic import BaseModel
 
 from ruamel.yaml import YAML
 
@@ -7,58 +9,83 @@ from dynamic_fusion.interactive_visualizer import Visualizer, VisualizerConfigur
 from dynamic_fusion.network_trainer import Trainer, TrainerConfiguration
 from dynamic_fusion.utils.seeds import set_seeds
 
-def generate_data_reconstruction(arguments: argparse.Namespace) -> None:
+
+def generate_data_reconstruction(arguments: argparse.Namespace, unknown_args: List[str]) -> None:
     # pylint: disable-next=import-outside-toplevel
     from dynamic_fusion.data_generator import DataGenerator, DataGeneratorConfiguration
 
     with open(arguments.config, encoding="utf8") as infile:
         yaml = YAML().load(infile)
         config = DataGeneratorConfiguration.parse_obj(yaml)
+        update_config(config, unknown_args)
     data_generator = DataGenerator(config)
     data_generator.run()
 
 
-def train_reconstruction(arguments: argparse.Namespace) -> None:
+def train_reconstruction(arguments: argparse.Namespace, unknown_args: List[str]) -> None:
     with open(arguments.config, encoding="utf8") as infile:
         yaml = YAML().load(infile)
         config = TrainerConfiguration.parse_obj(yaml)
+        update_config(config, unknown_args)
     network_trainer = Trainer(config)
     network_trainer.run()
 
-def visualize_interactive(arguments: argparse.Namespace) -> None:
+
+def visualize_interactive(arguments: argparse.Namespace, unknown_args: List[str]) -> None:
     with open(arguments.config, encoding="utf8") as infile:
         yaml = YAML().load(infile)
         config = VisualizerConfiguration.parse_obj(yaml)
+        update_config(config, unknown_args)
     network_trainer = Visualizer(config)
     network_trainer.run()
 
 
-def main(arguments: argparse.Namespace) -> None:
+def update_config(config: BaseModel, unknown_args: List[str]) -> None:
+    for arg in unknown_args:
+        if not arg.startswith("--"):
+            continue
+        if not "=" in arg:
+            continue
+        key, val = arg[2:].split("=")
+        keys = key.split(".")
+        update_nested_config(config, keys, val)
+
+
+def update_nested_config(config: BaseModel, key_list: List[str], value: Any) -> None:
+    """Recursively update nested dictionary."""
+    key = key_list[0]
+    if len(key_list) == 1:
+        setattr(config, key, value)
+    else:
+        if key not in config.__fields__.keys() or not isinstance(getattr(config, key), BaseModel):
+            raise ValueError(f"Error setting {key_list} to {value}")
+        update_nested_config(getattr(config, key), key_list[1:], value)
+
+
+def main(arguments: argparse.Namespace, unknown_args: List[str]) -> None:
     logging.basicConfig(level=logging.INFO)
 
     if arguments.generate_data:
-        return generate_data_reconstruction(arguments)
+        return generate_data_reconstruction(arguments, unknown_args)
     if arguments.train:
-        return train_reconstruction(arguments)
+        return train_reconstruction(arguments, unknown_args)
     if arguments.visualize:
-        return visualize_interactive(arguments)
+        return visualize_interactive(arguments, unknown_args)
     raise NotImplementedError()
 
 
-def parse_arguments() -> argparse.Namespace:
+def parse_arguments() -> Tuple[argparse.Namespace, List[str]]:
     parser = argparse.ArgumentParser()
     parser.add_argument("--config", required=True, type=str)
-    
+
     mode_group = parser.add_mutually_exclusive_group(required=True)
-    mode_group.add_argument(
-        "--generate_data", action="store_true", help="generate data"
-    )
+    mode_group.add_argument("--generate_data", action="store_true", help="generate data")
     mode_group.add_argument("--train", action="store_true", help="train the model")
     mode_group.add_argument("--visualize", action="store_true", help="interactively visualize model")
 
-    return parser.parse_args()
+    return parser.parse_known_args()
 
 
 if __name__ == "__main__":
-    args = parse_arguments()
-    main(args)
+    args, unknown_args = parse_arguments()
+    main(args, unknown_args)
