7df45a0caa8da7984a897c9217c26dbb26ae0dc0
David Tonderski <dtonderski@gmail.com>
2024-02-17 10:52:43 +0100

    feat: numbify superresolution bottleneck

--- dynamic_fusion/utils/superresolution.py
+++ dynamic_fusion/utils/superresolution.py
@@ -1,8 +1,8 @@
-from itertools import product
 from typing import Tuple
 
 import numpy as np
 from jaxtyping import Bool, Float, Int
+from numba import jit
 
 
 def generate_coords_2d(resolution: Tuple[int, int]) -> Float[np.ndarray, "X Y 2"]:
@@ -18,23 +18,33 @@ def generate_coords_1d(resolution: int) -> Float[np.ndarray, " N"]:
     return np.linspace(0, 1, resolution + 1)[1:] - 1 / (2 * resolution)
 
 
+@jit(cache=True)
+def find_nearest_pixels(
+    start_resolution: np.ndarray, end_resolution: Tuple[int, int], start_coords: np.ndarray, end_coords: np.ndarray
+) -> np.ndarray:
+    nearest_pixels = np.zeros((4, *end_resolution, 2))
+    for i_end in range(end_resolution[0]):
+        for j_end in range(end_resolution[1]):
+            end_coord = end_coords[i_end, j_end]
+            nearest_pixel = np.floor(end_coord * start_resolution).astype(np.int64)
+            vector_from_nearest_start_to_end = end_coord - start_coords[nearest_pixel[0], nearest_pixel[1]]
+            next_x, next_y = (vector_from_nearest_start_to_end > 0) * 2 - 1
+
+            nearest_pixels[0, i_end, j_end] = nearest_pixel
+            nearest_pixels[1, i_end, j_end] = np.array([nearest_pixel[0] + next_x, nearest_pixel[1]])
+            nearest_pixels[2, i_end, j_end] = np.array([nearest_pixel[0], nearest_pixel[1] + next_y])
+            nearest_pixels[3, i_end, j_end] = np.array([nearest_pixel[0] + next_x, nearest_pixel[1] + next_y])
+
+    return nearest_pixels
+
+
 def get_upscaling_pixel_indices_and_distances(
     start_resolution: Tuple[int, int], end_resolution: Tuple[int, int]
 ) -> Tuple[Int[np.ndarray, "4 XUpscaled YUpscaled 2"], Float[np.ndarray, "4 XUpscaled YUpscaled 2"], Bool[np.ndarray, "4 XUpscaled YUpscaled"]]:
     start_coords = generate_coords_2d(start_resolution)
     end_coords = generate_coords_2d(end_resolution)
-    nearest_pixels = np.zeros((4, *end_resolution, 2))
 
-    for i_end, j_end in product(range(end_resolution[0]), range(end_resolution[1])):
-        end_coord = end_coords[i_end, j_end]
-        nearest_pixel = np.floor(end_coord * start_resolution).astype(int)
-        vector_from_nearest_start_to_end = end_coord - start_coords[nearest_pixel[0], nearest_pixel[1]]
-        next_x, next_y = (vector_from_nearest_start_to_end > 0) * 2 - 1
-
-        nearest_pixels[0, i_end, j_end] = nearest_pixel
-        nearest_pixels[1, i_end, j_end] = np.array([nearest_pixel[0] + next_x, nearest_pixel[1]])
-        nearest_pixels[2, i_end, j_end] = np.array([nearest_pixel[0], nearest_pixel[1] + next_y])
-        nearest_pixels[3, i_end, j_end] = np.array([nearest_pixel[0] + next_x, nearest_pixel[1] + next_y])
+    nearest_pixels = find_nearest_pixels(np.array(start_resolution), end_resolution, start_coords, end_coords)
 
     x_out_of_bounds = np.logical_or(nearest_pixels[..., 0] < 0, nearest_pixels[..., 0] >= start_coords.shape[0])
     y_out_of_bounds = np.logical_or(nearest_pixels[..., 1] < 0, nearest_pixels[..., 1] >= start_coords.shape[1])
