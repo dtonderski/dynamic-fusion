d3e68708dadf518c3c40cb0d2bdcfd106b9f1643
David Tonderski <dtonderski@gmail.com>
2024-03-27 16:59:37 +0100

    feat: update test_e2vid_data script

--- dynamic_fusion/scripts/test_e2vid_data.py
+++ dynamic_fusion/scripts/test_e2vid_data.py
@@ -8,6 +8,7 @@ import pandas as pd
 import torch
 from jaxtyping import Float
 from torch import nn
+from skimage.transform import resize
 
 from dynamic_fusion.data_generator.configuration import EventDiscretizerConfiguration
 from dynamic_fusion.data_generator.event_discretizer import EventDiscretizer
@@ -20,30 +21,62 @@ from dynamic_fusion.utils.network import stack_and_maybe_unfold_c_list, to_numpy
 from dynamic_fusion.utils.superresolution import get_spatial_upscaling_output, get_upscaling_pixel_indices_and_distances
 from dynamic_fusion.utils.visualization import create_red_blue_cmap, img_to_colormap
 
-CHECKPOINT_DIR = Path("./runs/ready/00_st-un_st-interp_st-up_16")
-CHECKPOINT_NAME = "checkpoint_150000"
+MODEL = 'e2vid_exp'
+MODEL = 'e2vid_exp_uncertainty'
 
-EVENT_DATA_FILE = Path("./data/raw/e2vid/hdr_selfie.txt")
+if MODEL == 'e2vid_exp':
+    CHECKPOINT_DIR = Path("./runs/0323-new-dataset/01_st-un_st-interp_st-up/subrun_00")
+elif MODEL == 'e2vid_exp_uncertainty':
+    CHECKPOINT_DIR = Path("./runs/0323-new-dataset/00_st-un_st-interp_st-up_uncertainty-lpips/subrun_00")
 
-USE_VIDEO_TO_GENERATE_EVENTS = True
-INPUT_VIDEO = Path("./data/raw/e2vid/gnome_huawei_240fps.mp4")
+CHECKPOINT_NAME = "latest_checkpoint.pt"
+
+NAME = "selfie"
+
+if NAME == "dynamic":
+    EVENT_DATA_FILE = Path("./data_e2vid/raw/e2vid/dynamic_6dof.txt")
+elif NAME == "gnome":
+    EVENT_DATA_FILE = Path("./data_e2vid/raw/e2vid/gun_bullet_gnome.txt")
+elif NAME == "selfie":
+    EVENT_DATA_FILE = Path("./data_e2vid/raw/e2vid/hdr_selfie.txt")
+
+MAX_T = 5
+
+USE_VIDEO_TO_GENERATE_EVENTS = False
+
+# Simulated events
+if NAME == "dynamic":
+    INPUT_VIDEO = None
+elif NAME == "gnome":
+    INPUT_VIDEO = Path("./data_e2vid/raw/e2vid/gnome_huawei_240fps.mp4")
+elif NAME == "selfie":
+    INPUT_VIDEO = Path("./data_e2vid/raw/e2vid/selfie_huawei_30fps.mp4")
+
 EVS_EXPLORER_CONFIG = "configs/data_generator/simulator/evs_explorer.yml"
 DAVIS_CONFIG = "configs/data_generator/simulator/davis_model.yml"
 NUM_FRAMES = 150
-THRESHOLD = 1.08
+THRESHOLD = 1.3
 MIN_ILLUMINANCE = 650
 MAX_ILLUMINANCE = 12500
 DESIRED_WIDTH = 640
 
+# Other settings
+FRAME_SIZE = 0.06  # 200 ms
 FRAME_SIZE = 0.02  # 200 ms
+
+SCALE = 8
 BINS_PER_FRAME = 2
 TAUS_TO_EVALUATE = 5
-MAX_HEIGHT = None
+MIN_HEIGHT = None
+MIN_WIDTH = 550
+MAX_HEIGHT = 200
 MAX_WIDTH = None
-NAME = "selfie"
-OUTPUT_FPS = 250
-OUTPUT_VIDEO_PATH = Path(f"./results/e2vid/{NAME}_simulated={USE_VIDEO_TO_GENERATE_EVENTS}_framesize={FRAME_SIZE}_fps={OUTPUT_FPS}_threshold={THRESHOLD}.mp4")
 
+SPEED = 0.2
+OUTPUT_FPS = int(TAUS_TO_EVALUATE / FRAME_SIZE * SPEED)
+OUTPUT_VIDEO_PATH = Path(f"./results/upscaled/{MODEL}/{NAME}_simulated={USE_VIDEO_TO_GENERATE_EVENTS}_framesize={FRAME_SIZE}_scale={SCALE}.mp4")
+
+OUTPUT_VIDEO_PATH.parent.mkdir(parents=True,exist_ok=True)
 
 def main() -> None:
     device = torch.device("cuda")
@@ -53,7 +86,7 @@ def main() -> None:
         json_config = f.read()
     # Parse the JSON string back into a Configuration instance
     config = TrainerConfiguration.parse_raw(json_config)
-
+    print(config)
     # Load network
     config.network_loader.decoding_checkpoint_path = CHECKPOINT_DIR / CHECKPOINT_NAME
     config.network_loader.encoding_checkpoint_path = CHECKPOINT_DIR / CHECKPOINT_NAME
@@ -67,26 +100,35 @@ def main() -> None:
         events, height, width = get_events_from_video(INPUT_VIDEO, MAX_HEIGHT, MAX_WIDTH, NUM_FRAMES, DESIRED_WIDTH)
     else:
         # Load events
-        events, height, width = get_events_from_txt(EVENT_DATA_FILE, MAX_HEIGHT, MAX_WIDTH)
+        events, height, width = get_events_from_txt(EVENT_DATA_FILE, MIN_HEIGHT, MAX_HEIGHT, MIN_WIDTH, MAX_WIDTH, max_t = MAX_T)
 
+    print(events, height, width)
     # Discretize events
     max_timestamp = events.timestamp.max() + (FRAME_SIZE - events.timestamp.max() % FRAME_SIZE)
     number_of_temporal_bins = max_timestamp // FRAME_SIZE
     discretizer_config = EventDiscretizerConfiguration(number_of_temporal_bins=number_of_temporal_bins, number_of_temporal_sub_bins_per_bin=BINS_PER_FRAME)
     discretizer = EventDiscretizer(discretizer_config, max_timestamp=max_timestamp)
-    discretized_events_dict = discretizer.run({1: events}, (height, width))
-    discretized_events = discretized_events_dict[1]
+    discretized_events_dict = discretizer.run({THRESHOLD: events}, (height, width))
+    discretized_events = discretized_events_dict[THRESHOLD]
     print(discretized_events.event_polarity_sum.shape)
 
+    out_height, out_width = (int(height*SCALE), int(width*SCALE))
+
     # Get reconstruction
-    reconstruction = run_reconstruction(encoder, decoder, discretized_events, device, config.shared, (height, width), TAUS_TO_EVALUATE)
+    reconstruction = run_reconstruction(encoder, decoder, discretized_events, device, config.shared, (out_height, out_width), TAUS_TO_EVALUATE)
 
     # Save video
     size = list(reversed(reconstruction.shape[-2:]))
-    size[0] *= 2
+    if reconstruction.shape[1] == 1:
+        size[0] *= 2
+    else:
+        size[0] *= 3
     size = tuple(size)  # type: ignore
 
     colored_event_polarity_sums = img_to_colormap(to_numpy(discretized_events.event_polarity_sum.sum(dim=1)), create_red_blue_cmap(501))
+    if SCALE is not None and SCALE != 1:
+        resized_eps = [resize(color_eps, (out_height, out_width), order=0) for color_eps in colored_event_polarity_sums]
+        colored_event_polarity_sums = np.stack(resized_eps, axis=0)
 
     out = cv2.VideoWriter(str(OUTPUT_VIDEO_PATH), cv2.VideoWriter.fourcc(*"mp4v"), OUTPUT_FPS, size)
     ms_per_frame = max_timestamp / (TAUS_TO_EVALUATE * number_of_temporal_bins) * 1000
@@ -98,7 +140,12 @@ def main() -> None:
     position = (10, 50)  # Position of the text (bottom left corner)
 
     for i, frame in enumerate(reconstruction):
-        frame = frame[0, :, :, np.newaxis]
+        if frame.shape[0] == 2:
+            frame = np.concatenate([frame[0, :, :, np.newaxis], np.exp(frame[1, :, :, np.newaxis])], axis=1)
+        else:
+            frame = frame[0, :, :, np.newaxis]
+        
+        frame = np.clip(frame, 0, 1)
 
         # Flip vertically and rescale to 0-255
         frame_processed = (frame[::-1] * 255).astype(np.uint8)
@@ -112,11 +159,11 @@ def main() -> None:
 
         frame_with_events = np.concatenate(((colored_event_polarity_sums[i_event_frame, ::-1] * 255).astype(np.uint8), frame_processed), axis=1)
 
-        if USE_VIDEO_TO_GENERATE_EVENTS:
+        if USE_VIDEO_TO_GENERATE_EVENTS or NAME in ['dynamic']:
             frame_with_events = np.flip(frame_with_events, 0)
             frame_with_events = np.ascontiguousarray(frame_with_events)
 
-        cv2.putText(frame_with_events, f"Event frame={i_event_frame}, time={ms_per_frame*i:.0f} ms, events per pixel in frame: {events_per_pixel_per_frame: .2f}", position, font, font_scale, font_color, line_type)
+        cv2.putText(frame_with_events, f"Event frame={i_event_frame}, t={ms_per_frame*i:.0f} ms, EPPF: {events_per_pixel_per_frame: .2f}", position, font, font_scale, font_color, line_type)
 
         out.write(frame_with_events)
 
@@ -195,15 +242,18 @@ def run_reconstruction(
         return reconstruction_flat
 
 # TODO: just load this as a pd dataframe
-def get_events_from_txt(file: Path, max_height: Optional[int] = None, max_width: Optional[int] = None, min_t: Optional[int] = None, max_t: Optional[int] = None) -> Tuple[Events, int, int]:
+def get_events_from_txt(file: Path, min_height: Optional[int] = None, max_height: Optional[int] = None, min_width: Optional[int] = None,
+                         max_width: Optional[int] = None, min_t: Optional[int] = None, max_t: Optional[int] = None) -> Tuple[Events, int, int]:
     lines = file.read_text()
     lines_split = lines.split("\n")
 
     event_dict = {"timestamp": [], "x": [], "y": [], "polarity": []}
 
+    
     max_height = max_height if max_height is not None else np.inf  # type: ignore
     max_width = max_width if max_width is not None else np.inf  # type: ignore
-
+    min_height = min_height if min_height is not None else 0
+    min_width = min_width if min_width is not None else 0
     t_start = None
 
     for line in lines_split[: len(lines_split)]:
@@ -218,21 +268,20 @@ def get_events_from_txt(file: Path, max_height: Optional[int] = None, max_width:
             if min_t and float(t) - t_start < min_t:
                 continue
 
-
-            if int(y) < max_height and int(x) < max_width:  # type: ignore
-                event_dict["timestamp"].append(float(t))
-                event_dict["x"].append(int(x))
-                event_dict["y"].append(int(y))
+            if min_height <= int(y) and int(y) < max_height and min_width <= int(x) and int(x) < max_width:  # type: ignore
+                event_dict["timestamp"].append(float(t) - t_start)
+                event_dict["x"].append(int(x) - min_width)
+                event_dict["y"].append(int(y) - min_height)
                 event_dict["polarity"].append(int(p) > 0)
 
         if len(line.split(" ")) == 2:
-            width, height = [int(x) for x in line.split(" ")]
+            width, height = [int(x) for x in line.split(" ")]      
 
-    timestamps = [x - event_dict["timestamp"][0] for x in event_dict["timestamp"]]
-    event_dict["timestamp"] = timestamps
+    width = min(width, max_width)
+    height = min(height, max_height)
 
-    width = width if max_width > width else max_width  # type: ignore
-    height = height if max_height > height else max_height  # type: ignore
+    width = width - min_width
+    height = height - min_height
 
     return pd.DataFrame(event_dict), height, width
 
